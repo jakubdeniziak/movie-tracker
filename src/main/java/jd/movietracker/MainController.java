26d60a3a1da14450d7ceefd93bd32ac3b1f22461
jakubdeniziak <jakubdeniziak2@gmail.com>
2023-08-08 22:07:59 +0200

    Add home page with featured movies

--- src/main/java/jd/movietracker/MainController.java
+++ src/main/java/jd/movietracker/MainController.java
@@ -1,12 +1,23 @@
 package jd.movietracker;
 
+import jd.movietracker.movie.Movie;
+import jd.movietracker.movie.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping({"", "/", "/index"})
-    public String index() {
+    public String index(Model model) {
+        List<Movie> movies = movieService.getFeaturedToday();
+        model.addAttribute("featured_today", movies);
         return "index";
     }
 }
--- src/main/java/jd/movietracker/movie/MovieService.java
+++ src/main/java/jd/movietracker/movie/MovieService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.List;
 
 @Service
@@ -14,5 +15,20 @@ public class MovieService {
 
     public List<Movie> getAll() {
         return repository.getAll();
+    }
+
+    public List<Movie> getFeaturedToday() {
+        int numberOfFeaturedMovies = 2;
+        long seed = getSeedBasedOnTodayDate();
+        return repository.getRandomMovies(seed, numberOfFeaturedMovies);
+    }
+
+    private long getSeedBasedOnTodayDate() {
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR, 0);
+        today.set(Calendar.MINUTE, 0);
+        today.set(Calendar.SECOND, 0);
+        today.set(Calendar.MILLISECOND, 0);
+        return today.getTimeInMillis();
     }
 }
