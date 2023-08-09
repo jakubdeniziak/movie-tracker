4bf552bca5939d53ca878a8b70d5a5ec591595ba
jakubdeniziak <jakubdeniziak2@gmail.com>
2023-08-09 20:13:06 +0200

    Add a superclass for media items that movie inherits

---
+++ src/main/java/jd/movietracker/MediaItem.java
@@ -0,0 +1,14 @@
+package jd.movietracker;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class MediaItem {
+    protected UUID id;
+}
--- src/main/java/jd/movietracker/movie/Movie.java
+++ src/main/java/jd/movietracker/movie/Movie.java
@@ -1,7 +1,9 @@
 package jd.movietracker.movie;
 
+import jd.movietracker.MediaItem;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
@@ -9,8 +11,8 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Movie {
-    private UUID id;
+@EqualsAndHashCode(callSuper = true)
+public class Movie extends MediaItem {
     private String title;
     private String director;
     private String language;
